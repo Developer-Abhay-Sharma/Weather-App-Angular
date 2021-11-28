a5b608d43dbcad1a017aad5cb254f222cfebf3c6
Abhay Sharma <abhay-shar107@gmail.com>
2021-11-28 21:00:58 +0530

    fix: loading page animation

--- src/app/mainbody/mainbody.component.ts
+++ src/app/mainbody/mainbody.component.ts
@@ -1,17 +1,39 @@
+import { animate, animation, state, style, transition, trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-mainbody',
   templateUrl: './mainbody.component.html',
-  styleUrls: ['./mainbody.component.css']
+  styleUrls: ['./mainbody.component.css'],
+  animations: [
+    trigger('letterMove', [
+      state('center', style({
+        opacity: '1',
+      })),
+      state('gone', style({
+        opacity: '0',
+        animation: 'forword'
+      })),
+      transition('center => gone', [
+        animate('1s')
+      ])
+    ])
+  ]
 })
 export class MainbodyComponent implements OnInit {
 
   ngOnInit(): void {
     this.getLocation() 
   }
+  loadFinish:boolean = false
   isLoadingFinish(){
-    return false
+    // this.loadFinish = true   
+    let mainBodyDisplay = document.querySelector('.main-body') as HTMLElement
+    mainBodyDisplay?.classList.add('disapperAnimation')
+    setTimeout(() => {
+      mainBodyDisplay.style.display = 'none'
+      this.displayMainScreen()
+    }, 2000);
   }
   getLocation(): void{
     if (navigator.geolocation) {
@@ -20,23 +42,26 @@ export class MainbodyComponent implements OnInit {
           localStorage.setItem('userLocation', JSON.stringify(userLocation))
         });
     } else {
-       console.log("No support for geolocation")
+      //  console.log("No support for geolocation")
     }
     navigator.permissions.query({ name: 'geolocation' })
     .then(res => {this.permissionResponse = res; this.nextComponent()})
   }
 permissionResponse:any
+isStart:boolean = true
   nextComponent(){
-    console.log(this.permissionResponse);
+    // console.log(this.permissionResponse);
     
     if(this.permissionResponse.state == "granted"){
-      console.log("access granted")
+      // console.log("access granted")
       setTimeout(() => {
-        this.applicationAnimationStart()
+        console.log('animation Starts');
+        this.isStart = !this.isStart
+        this.isLoadingFinish()
       }, 3300);
     }
     else if (this.permissionResponse.state == "denied") {
-      console.log("access denied");
+      // console.log("access denied");
       
     }
     else if (this.permissionResponse.state == "prompt") {
@@ -46,8 +71,8 @@ permissionResponse:any
       
     }
   }
-  applicationAnimationStart(){
-    console.log("Animation Starts");
+  displayMainScreen(){
+    this.loadFinish = true
   }
 }
  
