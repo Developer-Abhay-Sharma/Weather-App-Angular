eae0f098af301177be37a3a691b05575b5f4c38b
Abhay <abhay.shar107@gmail.com>
2021-12-22 19:19:54 +0530

    change temperature value from float to temp

--- src/app/Weather/mid/mid.component.ts
+++ src/app/Weather/mid/mid.component.ts
@@ -36,6 +36,16 @@ export class MidComponent  {
         this.hourly = this.data2.hourly
         this.daily = this.data2.daily
         this.daily.splice(0,1)
+        for (let i = 0; i < this.daily.length; i++) {
+          let max = parseInt(this.daily[i].temp.max)
+          let min = parseInt(this.daily[i].temp.min)
+          this.daily[i].temp.max = max
+          this.daily[i].temp.min = min
+        }
+        for (let i = 0; i < this.hourly.length; i++) {
+          let temp = parseInt(this.hourly[i].temp)
+          this.hourly[i].temp = temp
+        }
         console.log(this.hourly)
         // console.log(this.daily)
         // console.log(this.data2)
@@ -64,6 +74,16 @@ export class MidComponent  {
         this.hourly = this.data2.hourly
         this.daily = this.data2.daily
         this.daily.splice(0,1)
+        for (let i = 0; i < this.daily.length; i++) {
+          let max = parseInt(this.daily[i].temp.max)
+          let min = parseInt(this.daily[i].temp.min)
+          this.daily[i].temp.max = max
+          this.daily[i].temp.min = min
+        }
+        for (let i = 0; i < this.hourly.length; i++) {
+          let temp = parseInt(this.hourly[i].temp)
+          this.hourly[i].temp = temp
+        }
         // console.log(this.data2)
       })
     }, 500);
@@ -89,6 +109,29 @@ export class MidComponent  {
     }
     if (check > 46) {
       daynumber+=1
+    }
+    if (daynumber == 0) {
+      return this.day = 'Sunday'
+    } else if (daynumber == 1) {
+      return this.day = 'Monday'
+    }else if (daynumber == 2) {
+      return this.day = 'Tuesday'
+    } else if (daynumber == 3) {
+      return this.day = 'Wednesday'
+    } else if (daynumber == 4) {
+      return this.day = 'Thrusday'
+    } else if (daynumber == 5) {
+      return this.day = 'Friday'
+    } else if (daynumber == 6) {
+      return this.day = 'Saturday'
+    } else {
+      return this.day = 'Error'
+    }
+  }
+  nextDays(i:number){
+    let daynumber = new Date().getDay()+i+1
+    if (daynumber > 6) {
+      daynumber = daynumber - 7
     }
     if (daynumber == 0) {
       return this.day = 'Sunday'
