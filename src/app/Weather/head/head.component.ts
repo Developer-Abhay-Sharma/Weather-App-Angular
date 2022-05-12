bca23462770b3e5008d5101137a8cd8ca51f3bbb
Abhay <abhay.shar107@gmail.com>
2022-05-12 23:13:50 +0530

    added pwa notification service but have to remove cors origin condition

--- src/app/Weather/head/head.component.ts
+++ src/app/Weather/head/head.component.ts
@@ -1,16 +1,41 @@
 import { Component, OnInit } from '@angular/core';
 import { SwPush } from '@angular/service-worker';
-
+import { NotificationService } from 'src/app/_services/notification.service';
 @Component({
   selector: 'app-head',
   templateUrl: './head.component.html',
-  styleUrls: ['./head.component.css']
+  styleUrls: ['./head.component.css'],
 })
 export class HeadComponent implements OnInit {
-
-  constructor() { }
+  private readonly publickey =
+    'BD6BwoDPGRKyVhUiFw8IKvLyViWA-frxae8rdnW62_SPCSmTa-lmQLNUAXjLkTrgd82niCil63ag-JN0e8X3zrM';
+  constructor(
+    private swPush: SwPush,
+    private notificationService: NotificationService
+  ) {}
 
   ngOnInit(): void {
+    this.pushSubscription();
+    this.swPush.messages.subscribe(message => console.log(message));
+    this.swPush.notificationClicks.subscribe(
+      ({action, notification}) =>{
+        window.open(notification.data.url)
+      })
   }
 
+  pushSubscription() {
+    if (!this.swPush.isEnabled) {
+      console.log('Notification is not enabled or blocked??');
+      return;
+    }
+    this.swPush
+    .requestSubscription({
+      serverPublicKey: this.publickey,
+    })
+    .then((sub) => {
+      this.notificationService.getNotification(sub).subscribe(data => console.log(data)
+      )
+    })
+    .catch((err) => console.log(err));
+  }
 }
