fdda75b217b0d057dfb98a268e4958d6faefb8eb
Abhay <abhay.shar107@gmail.com>
2022-05-13 19:13:48 +0530

    notification service logic completed

--- src/app/Weather/head/head.component.ts
+++ src/app/Weather/head/head.component.ts
@@ -8,25 +8,23 @@ import { NotificationService } from 'src/app/_services/notification.service';
   styleUrls: ['./head.component.css'],
 })
 export class HeadComponent implements OnInit {
-  private readonly publickey =
-    'BD6BwoDPGRKyVhUiFw8IKvLyViWA-frxae8rdnW62_SPCSmTa-lmQLNUAXjLkTrgd82niCil63ag-JN0e8X3zrM';
+  private readonly publicKey =
+    'BMKvL-2jcLkYmxjQ5kbbcUWOUtCjdsTrJDOkPrPGm6RPAilhhT_RaYJpM4cSZ3zRU0BaY72d2aVOhnTr9FYdEso';
   constructor(
     private swPush: SwPush,
-    private swUpdate:SwUpdate,
+    private swUpdate: SwUpdate,
     private notificationService: NotificationService,
-    private appRef:ApplicationRef
+    private appRef: ApplicationRef
   ) {
     this.checkUpdate();
   }
 
   ngOnInit(): void {
-    this.pushSubscription();
-    this.swPush.messages.subscribe(message => console.log(message));
-    this.swPush.notificationClicks.subscribe(
-      ({action, notification}) =>{
-        window.open(notification.data.url)
-      })
-      this.updateClient();
+    this.swPush.messages.subscribe((message) => console.log(message));
+    this.swPush.notificationClicks.subscribe(({ action, notification }) => {
+      window.open(notification.data.url);
+    });
+    this.updateClient();
   }
 
   pushSubscription() {
@@ -34,28 +32,45 @@ export class HeadComponent implements OnInit {
       console.log('Notification is not enabled or blocked??');
       return;
     }
+    // console.log(this.publickey);
+    this.swPush.unsubscribe().then((data) => {
+      console.log(data);
+      console.log('unsubscribe');
+      this.swPush
+      .requestSubscription({
+        serverPublicKey: this.publicKey,
+      })
+      .then((sub) => {
+        this.notificationService
+          .getNotification(sub)
+          .subscribe((data) => console.log(data));
+      })
+      .catch((err) => console.log(err));
+    }).catch(err => console.log(err)
+    )
     this.swPush
     .requestSubscription({
-      serverPublicKey: this.publickey,
+      serverPublicKey: this.publicKey,
     })
     .then((sub) => {
-      this.notificationService.getNotification(sub).subscribe(data => console.log(data)
-      )
+      this.notificationService
+        .getNotification(sub)
+        .subscribe((data) => console.log(data));
     })
     .catch((err) => console.log(err));
   }
 
-  updateClient(){
+  updateClient() {
     if (!this.swUpdate.isEnabled) {
-      console.log("not enabled");
+      console.log('not enabled');
       return;
-    } 
+    }
     this.swUpdate.available.subscribe((event) => {
       console.log(`current`, event.current, `available `, event.available);
       if (confirm('update available for the app please conform')) {
         this.swUpdate.activateUpdate().then(() => location.reload());
       }
-    })
+    });
     this.swUpdate.activated.subscribe((event) => {
       console.log(`current`, event.previous, `available `, event.current);
     });
--- src/app/_services/notification.service.ts
+++ src/app/_services/notification.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
@@ -8,15 +9,10 @@ import { environment } from 'src/environments/environment';
 export class NotificationService {
   
   notification = environment.url + '/notification'
-  // webPush = environment.url + '/webPushPublicKey'
   constructor(private http:HttpClient) { }
 
   getNotification(data:any){
     console.log(data);
     return this.http.post(this.notification, data)
   }
-
-  // getPublicKey(){
-  //   return this.http.get(this.webPush)
-  // }
 }
