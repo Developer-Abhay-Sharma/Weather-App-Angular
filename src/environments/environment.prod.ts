78aba2613c9b4f6309dae1d59a951d15cf03f724
Abhay <abhay.shar107@gmail.com>
2022-05-13 22:44:13 +0530

    minor change

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   // url: 'http://localhost:8000',
-  url : 'https://backed-for-all-apps.herokuapp.com/'
+  url : 'https://backed-for-all-apps.herokuapp.com'
 };
