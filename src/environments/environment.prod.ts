c7b9ea37f458e3391807bdf7455061e4dfa38fd0
deepak <deepaksinghthakur40505@gmail.com>
2023-01-25 00:13:45 +0530

    update url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   // url: 'http://localhost:8000',
-  url : 'https://odd-puce-crocodile-belt.cyclic.app '
+  url : 'https://odd-puce-crocodile-belt.cyclic.app'
 };
